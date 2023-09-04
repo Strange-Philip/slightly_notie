01b26fdaf840a12c8db836473a1f4fd471cf3bd1
Strange-Philip <philipabakahmensah@gmail.com.com>
2023-09-04 22:51:10 +0000

    added home screen widget

---
+++ ios/NotesWidgets/NotesWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NotesWidgetsBundle.swift
+//  NotesWidgets
+//
+//  Created by Philip Abakah on 21/07/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NotesWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NotesWidgets()
+    }
+}
